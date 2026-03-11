11a652f826a89bc23a71f3f67180737665883a92
Ignacio Atienza Crespo <ignacio@Ignacios-MacBook-Pro.local>
2026-03-11 12:17:16 +0800

    Gallery: add image_10, image_8 as sidebar, fix face cropping on images 2/4/10

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -6,14 +6,15 @@ interface GalleryItemProps {
   src: string;
   alt: string;
   className?: string;
+  imgClassName?: string;
 }
 
-const GalleryItem: React.FC<GalleryItemProps> = ({ src, alt, className = '' }) => (
+const GalleryItem: React.FC<GalleryItemProps> = ({ src, alt, className = '', imgClassName = '' }) => (
   <div className={`relative group overflow-hidden ${className}`}>
     <img
       src={src}
       alt={alt}
-      className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
+      className={`w-full h-full object-cover transition-transform duration-700 group-hover:scale-105 ${imgClassName}`}
     />
     <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none" />
   </div>
@@ -30,23 +31,25 @@ const Gallery: React.FC = () => {
           <p className="text-gray-500 font-script text-2xl">{t.gallery.subtitle}</p>
         </div>
 
-        <div className="grid grid-cols-3 gap-6">
-          {/* Fila 1: ancha + cuadrada */}
-          <GalleryItem src="./images/image_1.jpeg" alt="Momento 1" className="col-span-2 h-72" />
-          <GalleryItem src="./images/image_2.jpeg" alt="Momento 2" className="col-span-1 h-72" />
+        <div className="grid grid-cols-[4fr_1fr] gap-6">
+          {/* Left: mosaic grid */}
+          <div className="grid grid-cols-3 gap-6">
+            <GalleryItem src="./images/image_1.jpeg" alt="Momento 1" className="col-span-2 h-72" />
+            <GalleryItem src="./images/image_2.jpeg" alt="Momento 2" className="col-span-1 h-72" imgClassName="object-top" />
 
-          {/* Fila 2: tres iguales */}
-          <GalleryItem src="./images/image_3.jpeg" alt="Momento 3" className="col-span-1 h-64" />
-          <GalleryItem src="./images/image_4.png"  alt="Momento 4" className="col-span-1 h-64" />
-          <GalleryItem src="./images/image_5.jpeg" alt="Momento 5" className="col-span-1 h-64" />
+            <GalleryItem src="./images/image_3.jpeg" alt="Momento 3" className="col-span-1 h-64" />
+            <GalleryItem src="./images/image_4.png"  alt="Momento 4" className="col-span-1 h-64" imgClassName="object-top" />
+            <GalleryItem src="./images/image_5.jpeg" alt="Momento 5" className="col-span-1 h-64" />
 
-          {/* Fila 3: cuadrada + ancha */}
-          <GalleryItem src="./images/image_6.jpeg" alt="Momento 6" className="col-span-1 h-72" />
-          <GalleryItem src="./images/image_7.jpeg" alt="Momento 7" className="col-span-2 h-72" />
+            <GalleryItem src="./images/image_6.jpeg" alt="Momento 6" className="col-span-1 h-72" />
+            <GalleryItem src="./images/image_7.jpeg" alt="Momento 7" className="col-span-2 h-72" />
 
-          {/* Fila 4: ancha + cuadrada */}
-          <GalleryItem src="./images/image_8.jpeg" alt="Momento 8" className="col-span-2 h-72" />
-          <GalleryItem src="./images/image_9.jpeg" alt="Momento 9" className="col-span-1 h-72" />
+            <GalleryItem src="./images/image_10.png" alt="Momento 10" className="col-span-2 h-72" imgClassName="object-top" />
+            <GalleryItem src="./images/image_9.jpeg" alt="Momento 9" className="col-span-1 h-72" />
+          </div>
+
+          {/* Right: tall sidebar image */}
+          <GalleryItem src="./images/image_8.jpeg" alt="Momento 8" className="h-full" imgClassName="object-right" />
         </div>
       </div>
     </section>
