dcef3f55233877ba37745b795f3ccceeca839625
Ignacio Atienza Crespo <ignacio@Ignacios-MacBook-Pro.local>
2026-05-31 17:13:18 +0800

    feat: redesign gifts section

--- i18n/translations.ts
+++ i18n/translations.ts
@@ -74,8 +74,13 @@ export const translations = {
     registry: {
       heading: 'Regalos',
       text: 'Para nosotros, lo más importante es compartir este día con vosotros. Si de todas formas tenéis ilusión por hacernos un regalo, os dejamos aquí nuestro número de cuenta para vuestra comodidad:',
-      spanishBank: 'Banco español: Sabadell | ES95 0081 1288 9800 0125 1629',
-      hongKongBank: 'Banco hongkonés: HSBC | 061-375408-833',
+      spanishBankLabel: 'Banco español',
+      spanishBankName: 'Sabadell',
+      spanishBankValue: 'ES95 0081 1288 9800 0125 1629',
+      hongKongBankLabel: 'Banco hongkonés',
+      hongKongBankName: 'HSBC',
+      hongKongBankValue: '061-375408-833',
+      thanks: 'Gracias por formar parte de este día tan especial.',
     },
     // RSVP
     rsvp: {
@@ -182,8 +187,13 @@ export const translations = {
     registry: {
       heading: 'Gifts',
       text: 'The greatest gift to us is your presence on our special day. However, if you would like to honor us with a gift, we have provided our bank details below for your convenience:',
-      spanishBank: 'Spanish bank: Sabadell | ES95 0081 1288 9800 0125 1629',
-      hongKongBank: 'Hong Kong bank: HSBC | 061-375408-833',
+      spanishBankLabel: 'Spanish bank',
+      spanishBankName: 'Sabadell',
+      spanishBankValue: 'ES95 0081 1288 9800 0125 1629',
+      hongKongBankLabel: 'Hong Kong bank',
+      hongKongBankName: 'HSBC',
+      hongKongBankValue: '061-375408-833',
+      thanks: 'Thank you for being part of this very special day.',
     },
     rsvp: {
       title: 'R.S.V.P.',
@@ -288,8 +298,13 @@ export const translations = {
     registry: {
       heading: '禮物',
       text: '對我哋嚟講，最珍貴嘅禮物就係你哋喺呢個特別日子嘅出席。不過，如果你想送禮，我哋喺下面提供咗銀行資料方便你：',
-      spanishBank: '西班牙銀行：Sabadell | ES95 0081 1288 9800 0125 1629',
-      hongKongBank: '香港銀行：HSBC | 061-375408-833',
+      spanishBankLabel: '西班牙銀行',
+      spanishBankName: 'Sabadell',
+      spanishBankValue: 'ES95 0081 1288 9800 0125 1629',
+      hongKongBankLabel: '香港銀行',
+      hongKongBankName: 'HSBC',
+      hongKongBankValue: '061-375408-833',
+      thanks: '多謝你成為我哋呢個特別日子嘅一部分。',
     },
     rsvp: {
       title: 'R.S.V.P.',
